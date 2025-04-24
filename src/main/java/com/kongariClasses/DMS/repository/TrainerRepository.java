ebcfb7f67f4aa372dddeb8e399cad27004bf8536
Bhavani Kongari <s575201@nwmissouri.edu>
2025-04-24 15:38:20 -0500

    initial commit

---
+++ src/main/java/com/kongariClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,10 @@
+package com.kongariClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.kongariClasses.DMS.Models.Trainer;
+
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
